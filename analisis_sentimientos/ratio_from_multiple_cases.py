50b1df0d3eb5b9f1d17c02432f84ce2b66f70e30
Borja <boorjabs@gmail.com>
2023-05-15 21:30:31 +0200

    1

--- analisis_sentimientos/ratio_from_multiple_cases.py
+++ analisis_sentimientos/ratio_from_multiple_cases.py
@@ -1,35 +1,9 @@
-from faker import Faker
 from textblob import TextBlob
-from googletrans import Translator
-
 from analisis_sentimientos.domain.AnalyzedSentence import AnalyzedSentence
 from analisis_sentimientos.util import SentenceUtils
 from matplotlib_lib.MatPlotScatter.MatPlotScatter import MatPlotScatterData, MatPlotScatter
 
-fake = Faker()
-# sentences = [fake.sentence() for _ in range(10)] crear de forma aleatorio
-
-sentences = [
-    "I love spending time with my family.",
-    "The beach is my favorite place to relax.",
-    "I am really excited about my new job.",
-    "My favorite hobby is playing the guitar.",
-    "I always feel happy when I eat ice cream.",
-    "The movie I watched last night was very boring.",
-    "I am so tired of this rainy weather.",
-    "I can't wait to travel to Europe next summer.",
-    "I feel really stressed out about my upcoming exam.",
-    "My best friend always knows how to make me laugh."
-]
-
-if SentenceUtils.sentence_is_not_in_english(sentences):
-    translator = Translator()
-    translated_sentences = []
-
-    for sentence in sentences:
-        translated_sentences.append(translator.translate(sentence, src='en', dest='es'))
-
-    sentences = translated_sentences
+sentences = SentenceUtils.obtain_data()
 
 formalized_sentences = []
 
--- analisis_sentimientos/util/SentenceUtils.py
+++ analisis_sentimientos/util/SentenceUtils.py
@@ -1,2 +1,45 @@
+import time
+
+from langdetect import detect
+from googletrans import Translator
+
+
+def obtain_data():
+    sentences = populate_sentences()
+    if sentence_is_not_in_english(sentences):
+        translated_sentences = []
+        for sentence in sentences:
+            translated_sentences.append(translate(str(sentence), 'es', 'en'))
+        sentences = translated_sentences
+
+    return sentences
+
+
+def populate_sentences():
+    sentences = [
+        'Odio los lunes',
+        'Me encanta el peso muerto',
+        'Hola Mundo'
+    ]
+    return sentences
+
+
 def sentence_is_not_in_english(sentences):
-    return True
+    language = detect(sentences[0])
+    return language != 'en'
+
+
+def translate(sentence, src, dest):
+    time.sleep(1)
+    translator = Translator()
+    return translator.translate(sentence, src=src, dest=dest).text
+
+
+sentences = [
+    'Odio los lunes',
+    'Me encanta el peso muerto',
+    'Hola Mundo'
+]
+
+for s in sentences:
+    print(translate(s, 'es', 'en'))
